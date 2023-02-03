46505127738daca54e28f7cfce239b5a3da8dc1e
Piotr Solnica <piotr.solnica.ps@gmail.com>
2023-02-03 02:14:29 +0100

    toggle done button added and created bind function

--- JS/script.js
+++ JS/script.js
@@ -24,20 +24,12 @@
         render();
     };
 
-    const render = () => {
-        let htmlString = "";
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
 
-        for (const task of tasks) {
-            htmlString += `
-                <li>
-                <button class="js-remove"> usuń </button>
-                   ${task.content}
-                </li>
-            `;
-        };
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-
+    const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
 
         removeButtons.forEach((removeButtons, taskIndex) => {
@@ -46,6 +38,34 @@
             });
 
         });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, taskIndex) => {
+            toggleDoneButton.addEventListener("click", () => {
+               toggleTaskDone(taskIndex);
+            });
+
+        });
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li
+                ${task.done ? "style=\"text-decoration: line-through\"" : ""} >
+                <button class="js-done"> zrobione? </button>
+                <button class="js-remove"> usuń </button>
+                   ${task.content}
+                </li>
+            `;
+        };
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        bindEvents();
     };
 
     const onFormSubmit = (event) => {
